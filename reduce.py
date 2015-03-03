853fce97da3cf5c808532e6791c69a68e719f110
Aron <kymowind@gmail.com>
2015-03-03 23:01:08 +0800

    Update reduce.py

--- reduce.py
+++ reduce.py
@@ -3,34 +3,23 @@ import sys
 import copy
 N = 3
 def reduce():
-	last_key = ''
-	t = dict([(i + 1, 0) for i in range(N)])
-
+	last_key, t = '', dict([(i + 1, 0) for i in range(N)])
 	matrix = {'A' : copy.deepcopy(t),\
 			'B' : copy.deepcopy(t)}
-
 	for line in sys.stdin:
 		key, value = line.strip().split('\t')
 		i, k = [int(i) for i in key.split('_')]
 		tag, j, val = value.split('_')
-		j = int(j)
-		val = float(val)
+		j, val = int(j), float(val)
 		if last_key == '':
-			matrix[tag][j] = val
-			last_key = key
+			matrix[tag][j], last_key = val, key
 		else:
 			if last_key != key:
-				ret_val = 0
-				for k in t:
-					ret_val += (matrix['A'][k] * matrix['B'][k])
-				print ret_val
-				matrix = {'A' : copy.deepcopy(t), 'B' : copy.deepcopy(t)}
-				last_key = key
+				print sum([matrix['A'][k] * matrix['B'][k] for k in t])
+				matrix, last_key = {'A' : copy.deepcopy(t), 'B' : copy.deepcopy(t)}, key
 			matrix[tag][j] = val
-	ret_val = 0
-	for k in t:
-		ret_val += (matrix['A'][k] * matrix['B'][k])
-	print ret_val
-
+			
+	print sum([matrix['A'][k] * matrix['B'][k] for k in t])
+	
 if __name__ == '__main__':
 	reduce()
